a4f90b2d0d8a789bcaa495bd31cdc640399f114a
medanmilos1831 <medanmilos1831@gmail.com>
2024-07-23 00:25:18 +0200

    fix

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -11,35 +11,40 @@ import {
 import { createBrowserRouter } from 'react-router-dom';
 import { ProgramiPage } from 'src/pages/ProgramiPage';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: SPA_ROUTES.ABOUT_PAGE,
+          element: <AboutPage />,
+        },
+        {
+          path: `${SPA_ROUTES.PROGRAMS_PAGE}/:program`,
+          element: <ProgramPage />,
+        },
+        {
+          path: `${SPA_ROUTES.PROGRAMS_PAGE}/:program/:category`,
+          element: <CategoryPage />,
+        },
+        {
+          path: `${SPA_ROUTES.PROGRAMS_PAGE}/:program/:category/:product`,
+          element: <ProductPage />,
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <>nema</>,
+    },
+  ],
   {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: SPA_ROUTES.ABOUT_PAGE,
-        element: <AboutPage />,
-      },
-      {
-        path: `${SPA_ROUTES.PROGRAMS_PAGE}/:program`,
-        element: <ProgramPage />,
-      },
-      {
-        path: `${SPA_ROUTES.PROGRAMS_PAGE}/:program/:category`,
-        element: <CategoryPage />,
-      },
-      {
-        path: `${SPA_ROUTES.PROGRAMS_PAGE}/:program/:category/:product`,
-        element: <ProductPage />,
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <>nema</>,
-  },
-]);
+    basename: 'auto-delovi-3sp',
+  }
+);
