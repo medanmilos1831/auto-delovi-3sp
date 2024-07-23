a4d753a2654e4e0e02f21d4832eb862b7bd0e149
medanmilos1831 <medanmilos1831@gmail.com>
2024-07-24 00:14:24 +0200

    fix

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -20,77 +20,76 @@ import { RouterProvider } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>Coming soon....</>
-    // <StoreClientProvider
-    //   store={{
-    //     reducer: (state: any, action: any) => {
-    //       switch (action.type) {
-    //         case 'addProduct':
-    //           if (state.products.length === 0) {
-    //             state.products = [
-    //               ...state.products,
-    //               {
-    //                 qty: 1,
-    //                 product: action.payload,
-    //               },
-    //             ];
-    //           }
-    //           const index = state.products.findIndex(
-    //             (i: any) => i.product.id === action.payload.id
-    //           );
-    //           if (index > -1) {
-    //             return state;
-    //           } else {
-    //             state.products = [
-    //               ...state.products,
-    //               {
-    //                 qty: 1,
-    //                 product: action.payload,
-    //               },
-    //             ];
-    //           }
-    //           return state;
-    //         case 'setQyt':
-    //           state.products[action.payload.index].qty = action.payload.value;
-    //           return state;
-    //         case 'removeItem':
-    //           state.products.splice(action.payload, 1);
-    //           return state;
+    <StoreClientProvider
+      store={{
+        reducer: (state: any, action: any) => {
+          switch (action.type) {
+            case 'addProduct':
+              if (state.products.length === 0) {
+                state.products = [
+                  ...state.products,
+                  {
+                    qty: 1,
+                    product: action.payload,
+                  },
+                ];
+              }
+              const index = state.products.findIndex(
+                (i: any) => i.product.id === action.payload.id
+              );
+              if (index > -1) {
+                return state;
+              } else {
+                state.products = [
+                  ...state.products,
+                  {
+                    qty: 1,
+                    product: action.payload,
+                  },
+                ];
+              }
+              return state;
+            case 'setQyt':
+              state.products[action.payload.index].qty = action.payload.value;
+              return state;
+            case 'removeItem':
+              state.products.splice(action.payload, 1);
+              return state;
 
-    //         case 'clearProducts':
-    //           state.products = action.payload;
-    //           return state;
-    //         case 'openCard':
-    //           state.openCard = action.payload;
-    //           return state;
+            case 'clearProducts':
+              state.products = action.payload;
+              return state;
+            case 'openCard':
+              state.openCard = action.payload;
+              return state;
 
-    //         default:
-    //           return state;
-    //       }
-    //     },
-    //     state: {
-    //       products: [],
-    //       openCard: false,
-    //     },
-    //   }}
-    // >
-    //   <QueryClientProvider
-    //     client={
-    //       new QueryClient({
-    //         defaultOptions: {
-    //           queries: {
-    //             refetchOnWindowFocus: false,
-    //             retry: 0,
-    //           },
-    //         },
-    //       })
-    //     }
-    //   >
-    //     <ApiProvider value={new Axios()}>
-    //       <RouterProvider router={router} />
-    //     </ApiProvider>
-    //   </QueryClientProvider>
-    // </StoreClientProvider>
+            default:
+              return state;
+          }
+        },
+        state: {
+          products: [],
+          openCard: false,
+        },
+      }}
+    >
+      <QueryClientProvider
+        client={
+          new QueryClient({
+            defaultOptions: {
+              queries: {
+                refetchOnWindowFocus: false,
+                retry: 0,
+              },
+            },
+          })
+        }
+      >
+        <ApiProvider value={new Axios()}>
+          <RouterProvider router={router} />
+        </ApiProvider>
+      </QueryClientProvider>
+    </StoreClientProvider>
   );
 };
 
