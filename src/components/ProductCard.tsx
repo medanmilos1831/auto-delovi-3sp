c505bdf55a3af861cb739419439b5f2c7873ae41
medanmilos1831 <medanmilos1831@gmail.com>
2024-11-05 08:10:22 +0100

    prod fix

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,105 +19,50 @@ const ProductCard = ({
   return (
     <>
       <div
-        className="w-full card bg-white dark:bg-gray-800 h-full"
+        key={slug}
+        className="card group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
         onClick={() => {
           nav(slug);
         }}
       >
         <div
-          className="relative"
+          className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96"
           style={{
-            paddingTop: '50%',
             overflow: 'hidden',
           }}
         >
-          <div className="absolute top-0 left-0 h-full w-full">
-            {imageUrl ? (
-              <img
-                className="card-image h-full w-full"
-                src={imageUrl}
-                alt={slug}
-              />
-            ) : (
-              <div
-                className="flex items-center justify-center h-full mb-4 bg-gray-300 rounded dark:bg-gray-700"
-                style={{
-                  backgroundColor: 'black',
-                }}
-              >
-                <svg
-                  className="w-10 h-10 text-gray-200 dark:text-gray-600"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 20"
-                >
-                  <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
-                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
-                </svg>
-              </div>
-            )}
-          </div>
-          <div
-            className="overlay"
-            style={{
-              zIndex: 2,
-            }}
-          ></div>
-        </div>
-        <div
-          className="p-5"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div className="flex justify-between mb-5 w-full">
-            <div
-              className="w-full"
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <div
-                style={{
-                  width: '100%',
-                }}
-              >
-                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white uppercase">
-                  {naziv}
-                </h5>
-              </div>
-              <div>
-                <span className="text-white">Kat. broj: </span>
-                <span className="font-bold tracking-tight text-gray-900 dark:text-white uppercase">
-                  {/* {cena} RSD */}
-                  {kataloskiBroj}
-                </span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <span className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg hover:bg-blue-800 bg-blue-700">
-              Detaljnije
+          {imageUrl ? (
+            <img
+              src={imageUrl}
+              className="card-image h-full w-full object-cover object-center sm:h-full sm:w-full"
+            />
+          ) : (
+            <div className="flex items-center justify-center h-full mb-4 bg-gray-300 rounded dark:bg-gray-700">
               <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+                fill="currentColor"
+                viewBox="0 0 16 20"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
+                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
               </svg>
-            </span>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-1 flex-col space-y-2 p-4">
+          <h3 className="text-sm font-medium text-gray-900">
+            <a>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {naziv}
+            </a>
+          </h3>
+          <div className="flex flex-1 flex-col justify-end">
+            <p className="text-sm italic text-gray-500">
+              Kataloški broj: {kataloskiBroj}
+            </p>
+            <p className="text-base font-medium text-gray-900">{cena} RSD</p>
           </div>
         </div>
       </div>
--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -7,51 +7,53 @@ const CategoryPage = () => {
     category: string;
   }>();
   return (
-    <PageTemplate
-      url={`category/${program}/${category}`}
-      queryKey="category_products"
-      select={(data: any) => {
-        return {
-          ...data,
-          items: data.products,
-        };
-      }}
-      hasSearch
-      onSearch={(value: any, state: any, setState: any) => {
-        let cloneState = state;
-        let v = state.items;
-        if (value) {
-          v = state.products.filter((item: any) =>
-            item.kataloski_broj.startsWith(value)
+    <>
+      <PageTemplate
+        url={`category/${program}/${category}`}
+        queryKey="category_products"
+        select={(data: any) => {
+          return {
+            ...data,
+            items: data.products,
+          };
+        }}
+        hasSearch
+        onSearch={(value: any, state: any, setState: any) => {
+          let cloneState = state;
+          let v = state.items;
+          if (value) {
+            v = state.products.filter((item: any) =>
+              item.kataloski_broj.startsWith(value)
+            );
+            setState((prev: any) => {
+              return {
+                ...prev,
+                items: v,
+              };
+            });
+          } else {
+            setState((prev: any) => {
+              return {
+                ...prev,
+                items: cloneState.products,
+              };
+            });
+          }
+        }}
+        render={(item: any) => {
+          return (
+            <ProductCard
+              caption={item.caption}
+              naziv={item.naziv}
+              cena={item.cena}
+              imageUrl={item.image}
+              slug={item.slug}
+              kataloskiBroj={item.kataloski_broj}
+            />
           );
-          setState((prev: any) => {
-            return {
-              ...prev,
-              items: v,
-            };
-          });
-        } else {
-          setState((prev: any) => {
-            return {
-              ...prev,
-              items: cloneState.products,
-            };
-          });
-        }
-      }}
-      render={(item: any) => {
-        return (
-          <ProductCard
-            caption={item.caption}
-            naziv={item.naziv}
-            cena={item.cena}
-            imageUrl={item.image}
-            slug={item.slug}
-            kataloskiBroj={item.kataloski_broj}
-          />
-        );
-      }}
-    />
+        }}
+      />
+    </>
   );
 };
 
